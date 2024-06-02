387fd043fc313c11b5b68bf995a628e57d237afc
Abuzar Asif <107269011+Abuzarasif@users.noreply.github.com>
2024-06-03 01:14:50 +0500

    Add files via upload

---
+++ 12 js tasks/buttondiv.js
@@ -0,0 +1,26 @@
+// const mover=(button)=>{
+//     const div= document.getElementById("destination")
+// div.appendChild(button)
+// }
+
+// const dmover=(button)=>{
+//     const div= document.getElementById("source")
+// div.appendChild(button)
+// }
+
+
+const movea=()=>{
+   let buta= document.getElementById("one")
+   let dest= document.getElementById("destination")
+   dest.appendChild(buta)
+}
+const moveb=()=>{
+   let buta= document.getElementById("two")
+   let dest= document.getElementById("destination")
+   dest.appendChild(buta)
+}
+const movec=()=>{
+   let buta= document.getElementById("three")
+   let dest= document.getElementById("destination")
+   dest.appendChild(buta)
+}
